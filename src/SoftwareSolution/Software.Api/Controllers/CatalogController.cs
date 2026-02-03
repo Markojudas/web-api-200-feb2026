51d1cde965e26efdc9ed56b5e31a4b83df86d6f8
Jose R Hernandez <jose@markojudas.com>
2026-02-03 13:51:04 -0500

    update

--- src/SoftwareSolution/Software.Api/Controllers/CatalogController.cs
+++ src/SoftwareSolution/Software.Api/Controllers/CatalogController.cs
@@ -24,6 +24,14 @@ public static class CatalogApiExtensions
 
             group.MapPost("", async (CatalogItemRequest req) =>
             {
+                // do not create references to backing services EVER EVER EVER
+                using var client = new HttpClient();
+
+                var request = new HttpRequestMessage(HttpMethod.Get, "https://work-share.akita-velociraptor.ts.net/vendors/123e4567-e89b-12d3-a456-426614174000");
+
+                using var response = await client.SendAsync(request);
+                // if the vendor you supplied isn't supported, I'm giong to return an error.
+                // a 400? weird.
                 return TypedResults.Ok(req);
             });
                
@@ -34,8 +42,9 @@ public static class CatalogApiExtensions
 
 public record CatalogItemRequest
 {
-    [Required]
+    [Required, MinLength(3), MaxLength(100)]
     public string Title { get; set; } = string.Empty;
     [Required]
     public string VendorId { get; set; } = string.Empty;
+
 }
